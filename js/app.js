56b6fc62a16ade2c48b188427620b60de62277be
Vanza Setia <venusbumi2@gmail.com>
2022-10-21 17:05:34 +0700

    Treat the menu as modal
    This commit makes the menu as modal. So, the users can only focus on the
    interactive elements inside the menu. Also, it disables scrolling on the
    page.

--- js/app.js
+++ js/app.js
@@ -4,17 +4,33 @@
   const menuButton = document.querySelector(".js-menu-button");
   const menuList = document.querySelector(".js-menu-list");
   const closeMenuButton = document.querySelector(".js-close-menu-button");
+  const headerLinks = document.querySelectorAll(".js-header-link");
+  const main = document.querySelector(".js-main");
+  const footer = document.querySelector(".js-footer");
+  const onLargeScreen = window.matchMedia("(min-width: 40em)");
+
+  const trapFocusOnNav = () => {
+    const elements = [...headerLinks, main, footer];
+    elements.forEach((element) => element.setAttribute("inert", ""));
+  };
+
+  const removeInertAttributes = () => {
+    const elements = [...headerLinks, main, footer];
+    elements.forEach((element) => element.removeAttribute("inert"));
+  };
 
   const openMenu = () => {
     menuButton.setAttribute("aria-expanded", "true");
     menuList.classList.add("is-open");
     body.classList.add("is-menu-open");
+    trapFocusOnNav();
   };
 
   const closeMenu = () => {
     menuButton.setAttribute("aria-expanded", "false");
     menuList.classList.remove("is-open");
     body.classList.remove("is-menu-open");
+    removeInertAttributes();
   };
 
   const toggleMenu = () => {
@@ -28,4 +44,5 @@
 
   menuButton.addEventListener("click", toggleMenu);
   closeMenuButton?.addEventListener("click", closeMenu);
+  onLargeScreen.addEventListener("change", closeMenu);
 })();
